7cb6e0680289d6ac35af88fb6a9583818dd7d4f7
Sherin Jose <sherinjose196@gmail.com>
2020-11-12 12:53:42 +0530

    zoom meetings

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,25 +3,32 @@
 namespace App\Http\Controllers;
 use MacsiDigital\Zoom\Zoom;
 use Illuminate\Http\Request;
-
+use Carbon\Carbon;
 class HomeController extends Controller
 {
     public function zoom()
     {
+
+        
         $zoom = new \MacsiDigital\Zoom\Support\Entry;
 
         $user = new \MacsiDigital\Zoom\User($zoom);
         $users = $user->find('sherin.jose@logezy.com');
-      
-        $zoom_meet = $zoom->meeting;
         
+        $zoom_meet = $users->meetings;
+      
+        
+        
+       
+          $new = $user->find('21lGz-gMTgehAX8aDR6I1g')->meetings; 
+          //dd($new);
+          $new->create([
+            'topic' => 'Introo',
+            'type' => 2,
+            'start_time' => new Carbon('2020-08-12 10:00:00'),
+          ]); 
        // $zoom_meet->create($users->id);
-        $zoom_meeting =  $user->create([
-            'first_name' => 'First Name',
-            'last_name' => 'Last Name',
-            'email' => 'sherin.jose@logezy.com',
-            'password' => '12345678990'
-        ]); 
-dd($zoom_meeting);
+        
+
     }
 }
